410fb0330d1b9ec8e40b2844af0e5bead769954e
estefaniagPerez <este.gutierrez.perez@gmail.com>
2025-12-23 00:47:14 +0100

    clean project

--- src/internal/parser/interface.go
+++
@@ -1,7 +0,0 @@
-package parser
-
-// Manager is the generic interface for any parsing implementation.
-type Manager[T any] interface {
-	Write(file string, data T) error
-	Read(file string) (T, error)
-}
